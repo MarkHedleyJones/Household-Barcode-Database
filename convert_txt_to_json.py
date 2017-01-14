6b1f2bb48a0d3e2c37317e98b24e39ddd6215fc5
Mark H. Jones <markjones112358@gmail.com>
2017-01-15 08:56:51 +1300

    Handle empty lines

--- convert_txt_to_json.py
+++ convert_txt_to_json.py
@@ -7,8 +7,9 @@ with open('source.txt', 'r') as f:
 	out = f.readlines()
 
 for line in out:
-	parts = line.replace('\n','').split(';')
-	codes[parts[1]] = parts[0]
+    if line.find(';') != -1:
+        parts = line.replace('\n','').split(';')
+        codes[parts[1]] = parts[0]
 
 a = json.dumps(codes, sort_keys=True, indent=4)
 with open('barcodes.json', 'w') as f:
